58b1eabcc5379c16107c77fba5ca1c8f9f05827b
Jason Jordan <jasonajordan0@gmail.com>
2020-09-02 19:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ function theBeatlesPlay (array1, array2){
   let newArray = [];
   
   for (let i = 0; i < array1.length; i++){
-    
+      let musican = array1[i];
+      let instrument = array2[i];
+      
+      newArray.push(musican + ' plays ' + instrument);
     
   }
   

3bca3292edb954e4540d4ed44168a287115070f6
Jason Jordan <jasonajordan0@gmail.com>
2020-09-02 19:32:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ function theBeatlesPlay (array1, array2){
   for (let i = 0; i < array1.length; i++){
       let musican = array1[i];
       let instrument = array2[i];
-      
       newArray.push(musican + ' plays ' + instrument);
-    
+  }
+  
+  return newArray;
+}
+
+function johnLennonFacts (array){
+  let newArray = [];
+  
+  for (let i = 0; i < array.length; i++){
+      let fact = array[i];
+      let fixFact = fact + "!!!";
+      newArray.push(fixFact);
   }
   
   return newArray;
